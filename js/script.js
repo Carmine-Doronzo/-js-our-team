45c84b8869e81a37bf914ec5f1f47a461fb9817e
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-03-18 14:59:33 +0100

    Bonus 2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //Function
-function generateMember(memberName, memberRule, memberImage){
+function generateMember(memberName, memberRule, memberImage) {
 
     const member = {
         name: memberName,
         rule: memberRule,
         image: memberImage,
-        
+
     };
 
     return member;
@@ -17,18 +17,18 @@ console.log('JS our Team');
 /*MILESTONE 0:
 Creare l’array di oggetti con le informazioni fornite. */
 
-const members =[];
+const members = [];
 
-const wayne = generateMember('Wayne Barnett','Founder & CEO','./img/wayne-barnett-founder-ceo.jpg');
-const angela = generateMember('Angela Caroll','Chief Editor','./img/angela-caroll-chief-editor.jpg');
-const walter = generateMember('Walter Gordon','Office Manager','./img/walter-gordon-office-manager.jpg');
-const lopez = generateMember('Angela Lopez','Social Media Manager','./img/angela-lopez-social-media-manager.jpg');
-const scott = generateMember('Scott Estrada','Developer','./img/scott-estrada-developer.jpg');
-const barbara = generateMember('Barbara Ramos','Graphic Designer','./img/barbara-ramos-graphic-designer.jpg');
+const wayne = generateMember('Wayne Barnett', 'Founder & CEO', './img/wayne-barnett-founder-ceo.jpg');
+const angela = generateMember('Angela Caroll', 'Chief Editor', './img/angela-caroll-chief-editor.jpg');
+const walter = generateMember('Walter Gordon', 'Office Manager', './img/walter-gordon-office-manager.jpg');
+const lopez = generateMember('Angela Lopez', 'Social Media Manager', './img/angela-lopez-social-media-manager.jpg');
+const scott = generateMember('Scott Estrada', 'Developer', './img/scott-estrada-developer.jpg');
+const barbara = generateMember('Barbara Ramos', 'Graphic Designer', './img/barbara-ramos-graphic-designer.jpg');
 
 //console.log(wayne)
 
-members.push(wayne,angela,walter,lopez,scott,barbara);
+members.push(wayne, angela, walter, lopez, scott, barbara);
 
 console.log(members)
 
@@ -36,26 +36,31 @@ console.log(members)
 Stampare le stesse informazioni su DOM semplicemente come stringhe */
 const strigDomElement = document.querySelector('.row');
 
-for(let i = 0; i < members.length; i++){
+for (let i = 0; i < members.length; i++) {
 
-    strigDomElement.innerHTML +=`
+    strigDomElement.innerHTML += `
     
     <div class="col-4">
-        <img src="${members[i].image}">
-        <div>${members[i].name}</div>
-        <div>${members[i].rule}</div> <br>
+        <div class="card" style="width: 18rem;">
+            <img src="${members[i].image}" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-text">${members[i].name}</h2>
+                <h6 class="card-text">${members[i].rule}</h2>
+            </div>
+        </div>
+        
                
     </div>
     
     `
-        
+
 
 }
 
 /*MILESTONE 1:
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto*/
 
-for(let i = 0; i < members.length; i++){
+for (let i = 0; i < members.length; i++) {
 
     console.log(members[i].name);
     console.log(members[i].rule);
